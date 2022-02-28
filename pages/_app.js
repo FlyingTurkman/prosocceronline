01fb5e923382acc16aa41a8cca6e2850ff28254e
SharkmanTR <denizxman@hotmail.com>
2022-02-28 03:25:22 +0300

    master

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,11 @@ import 'bootstrap/dist/css/bootstrap.css';
 import '../styles/globals.css'
 import Menu from '../components/menu';
 import cookies from 'js-cookie';
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
+
+
 const lang = cookies.get("lang")||"en";
 
 function MyApp({ Component, pageProps }) {
